d4e99018b882e444b8bdcdd8d79fbd583b53483b
modMars <malejandrosalcido@gmail.com>
2023-01-25 00:06:14 -0600

    Reorganize functions to achieve better legibility

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ start.addEventListener("click", (e) => {
   let saveName = prompt("Please type your name!");
   const player = Player(saveName);
   const CPU = Player("CPU");
-  displayRefresher();
+  start.className = "start disable";
+  gameBoard.boardSetter();
+});
+
+restart.addEventListener("click", (e) => {
+  displayManager.clearBoard();
+  container.className = "container";
+  start.className = "start";
+});
+
+suich.addEventListener("click", (e) => {
+  console.table(gameBoard.board);
 });
 
 /* gameBoard module, it's basically a factory function that calls itself at the end of it's definition, later on i can call any method i created within the gameBoard factory by just writing gameBoard.method();. */
@@ -17,11 +28,50 @@ const gameBoard = (() => {
     ["", "", ""],
     ["", "", ""],
   ];
-  const addMarker = () => console.table(board);
   /* winChecker consists of a function that checks whether or not someone has a winning combination on the board, this function gets called everytime a player makes a move. */
-  const winChecker = () => {
+  const boardSetter = () => {
+    let row = 0;
+    let column = 0;
+    const { board } = gameBoard;
+    board.forEach((e) => {
+      e.forEach((e) => {
+        const div = document.createElement("div");
+        div.setAttribute("data-row", row);
+        div.setAttribute("data-column", column);
+        div.className = "gameBoardSquare";
+        div.textContent = e;
+        div.addEventListener("click", (e) => {
+          if (board[e.target.dataset.row][e.target.dataset.column] == "") {
+            e.target.textContent = "X";
+            board[e.target.dataset.row][e.target.dataset.column] = "X";
+            gameFlow.winCheck();
+          } else if (
+            board[e.target.dataset.row][e.target.dataset.column] == ""
+          ) {
+            e.target.textContent = "O";
+            board[e.target.dataset.row][e.target.dataset.column] = "O";
+            gameFlow.winCheck();
+          }
+        });
+        container.appendChild(div);
+        column++;
+        if (column > 2) {
+          column = 0;
+          row++;
+        }
+      });
+    });
+  };
+
+  return { board, boardSetter };
+})();
+
+const gameFlow = (() => {
+  const winCheck = () => {
+    let { board } = gameBoard;
     // Define the winner variable.
     let winner = "none";
+    console.log(winner);
     /* the winChecks array consists of a multidimensional array containing every possible win condition in Tic Tac Toe. */
     let winChecks = [
       [board[0][0], board[0][1], board[0][2]],
@@ -45,58 +95,46 @@ const gameBoard = (() => {
       if (winChecks[i].every(XWins)) {
         winner = "X";
         display.textContent = `Winner: ${winner}`;
-        container.className += " disable";
+        container.className = "container disable";
+        console.log("winner x");
+        console.table(gameBoard.board);
       } else if (winChecks[i].every(OWins)) {
         winner = "O";
         display.textContent = `Winner: ${winner}`;
-        container.className += " disable";
+        container.className = "container disable";
+        console.log("winner o");
+        console.table(gameBoard.board);
       } else if (
         board[0].every(Tie) &&
         board[1].every(Tie) &&
         board[2].every(Tie)
       ) {
         winner = "Tie";
+        console.log("tie");
         display.textContent = `Winner: ${winner}`;
-        container.className += " disable";
+        container.className = "container disable";
       }
     }
     return winner;
   };
 
-  return { board, addMarker, winChecker };
+  return { winCheck };
 })();
 
-const displayRefresher = () => {
-  let row = 0;
-  let column = 0;
-  const { board } = gameBoard;
-  board.forEach((e) => {
-    e.forEach((e) => {
-      const div = document.createElement("div");
-      div.setAttribute("data-row", row);
-      div.setAttribute("data-column", column);
-      div.className = "gameBoardSquare";
-      div.textContent = e;
-      div.addEventListener("click", (e) => {
-        if (board[e.target.dataset.row][e.target.dataset.column] == "") {
-          e.target.textContent = "X";
-          board[e.target.dataset.row][e.target.dataset.column] = "X";
-          gameBoard.winChecker();
-        } else if (board[e.target.dataset.row][e.target.dataset.column] == "") {
-          e.target.textContent = "O";
-          board[e.target.dataset.row][e.target.dataset.column] = "O";
-          gameBoard.winChecker();
-        }
-      });
-      container.appendChild(div);
-      column++;
-      if (column > 2) {
-        column = 0;
-        row++;
-      }
-    });
-  });
-};
+const displayManager = (() => {
+  const clearBoard = () => {
+    gameBoard.board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+    while (container.hasChildNodes()) {
+      container.removeChild(container.firstChild);
+    }
+  };
+
+  return { clearBoard };
+})();
 
 const Player = (name) => {
   this.name = name;
